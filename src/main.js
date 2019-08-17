9374808aa24b51f7fc4e841c4ad8efe6fde3a0e0
ctj <3264275716@qq.com>
2019-08-17 10:39:37 +0800

    1.1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入全局css样式
+import './assets/css/normalize.css'
+import './assets/css/base.css'
+//配置axios
+import axios from 'axios'
+axios.defaults.baseURL="http://127.0.0.1:8080";
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+Vue.prototype.axios=axios;
+//引入Mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
 
 Vue.config.productionTip = false
 
